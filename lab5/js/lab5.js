4072eb33bcc178ba9bc50b6c7ace8834634bd14a
fatimaguz9 <fafaguz2004@gmail.com>
2024-10-20 15:51:56 -0700

    java and index html for lab5

---
+++ lab5/js/lab5.js
@@ -0,0 +1,21 @@
+/*
+ * index.js - Lab 5 Data Types and Variables
+ * Author: Fatima G
+ * Created: 10.20.2024
+*/
+
+// Define Variables 
+make = "Kia";
+model= "Sorento";
+color= "white";
+year= 2003;
+
+//calculate
+age = 2024 - year;
+
+//output
+document.writeln("Make:" + make + "<br");
+document.writeln("Model:" + model + "<br");
+document.writeln("Color:" + color + "<br");
+document.writeln("Year:" + year + "<br");
+document.writeln("Age:" + age + "years<br");
