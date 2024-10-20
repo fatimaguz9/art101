e34038259cce4b4ea2d5a30499e056338c395aa5
fatimaguz9 <fafaguz2004@gmail.com>
2024-10-20 16:59:55 -0700

    all updated lab 5

--- lab5/js/lab5.js
+++ lab5/js/lab5.js
@@ -6,16 +6,16 @@
 
 // Define Variables 
 make = "Kia";
-model= "Sorento";
-color= "White";
-year= 2003;
+model = "Sorento";
+color = "White";
+year = 2003;
 
 //calculate
 age = 2024 - year;
 
 //output
-document.writeln("Make:" + make + "<br");
-document.writeln("Model:" + model + "<br");
-document.writeln("Color:" + color + "<br");
-document.writeln("Year:" + year + "<br");
-document.writeln("Age:" + age + "years<br");
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "years<br>");
