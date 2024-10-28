384612034ed96a694af595bfbb91e624cac155ad
fatimaguz9 <fafaguz2004@gmail.com>
2024-10-28 11:15:37 -0700

    Update lab7.js

--- lab7/js/lab7.js
+++ lab7/js/lab7.js
@@ -7,7 +7,7 @@
 // Functions
 
 function sortUserName() {
-  let person =  prompt("Hi. What is your name?");
+  var person =  prompt("Hi. What is your name?");
   console.log("userName=", userName);
 
   var nameArray = userName.split('');
