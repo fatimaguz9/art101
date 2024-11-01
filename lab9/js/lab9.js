c996d63eb8a164e45facd8bc068bdabf3d3ab7c6
fatimaguz9 <fafaguz2004@gmail.com>
2024-11-01 12:39:18 -0700

    java and html

--- lab9/js/lab9.js
+++ lab9/js/lab9.js
@@ -1,16 +1,19 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/* 
+lab.js - This simple JavaScript/jQuery script uses buttons to modify some elements on the page
+ Requirements: jQuerry must be loaded for this script to work.
+Author: Fatima Guzman
+Date: 11.1.2024
+*/
 
-// Constants
+//add button to challenge section
+$("#challenge").append("<button id='button-challenge'>Make Special</button"); 
+
+//add a click listener to the challenge button
+$("button-challenge").click(function(){
+  $("#challenge").toggleClass("special");
+});
 
 // Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
 
 function main() {
   console.log("Main function started.");
