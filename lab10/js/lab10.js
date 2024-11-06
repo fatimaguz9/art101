187dcb9263f07e388ae0c46f4ba6fdea83ce783f
fatimaguz9 <fafaguz2004@gmail.com>
2024-11-05 21:31:32 -0800

    AHHHHH

--- lab10/js/lab10.js
+++ lab10/js/lab10.js
@@ -9,9 +9,23 @@ Date: 11.7.2024
 
 // Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-$("#button-challenge").click(function(){
+function generateRandomText() {
+  const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+  const min = 3;
+  const max = 100;
+  const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+  // Get a random starting index to slice the Lorem Ipsum text
+  const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+  // Generate the random Lorem Ipsum-like text
+  return text.slice(randStart, randStart + randLen);
+}
+
+// click listener for button
+$("#buttin").click(function(){
   $("#results").toggleClass("special");
 });
+
+const newText = generateRandomText();
+$("#output").append('<div class="text"><p>' + newText + '</p></div>');
 
 document.getElementById('myButton').addEventListener('click', handleClick);
