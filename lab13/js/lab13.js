6c3c1e8807bef0bd8953efaddab83c8ba25eb99f
fatimaguz9 <fafaguz2004@gmail.com>
2024-11-18 10:14:39 -0800

    trying to make it work

--- lab13/js/lab13.js
+++ lab13/js/lab13.js
@@ -9,12 +9,13 @@
 // Functions
 function fizzBuzz() {
   let output = '';
-for (i = 1; i <= 200; i++) {
-  let str = '';
-  if (i % 3 == 0) str += 'Fizz';
-   else if (i % 5 == 0) str += 'Buzz';
-   else if (i % 7 == 0) str += 'Boom';
+  for (let i = 1; i <= 200; i++) {
+    let str = '';
+    if (i % 3 == 0) str += 'Fizz';
+    if (i % 5 == 0) str += 'Buzz';
+    if (i % 7 == 0) str += 'Boom';
 
-   oneLongString += num + "Fizz!<br>";
-}
+    // Add number or some string
+    oneLongString += i + ((str !== '') ? " " + str : "") + "<br>";
+ }
 }
