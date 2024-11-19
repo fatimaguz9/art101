bf905b3978a2e42c66f379adb6782ec36368cde8
fatimaguz9 <fafaguz2004@gmail.com>
2024-11-18 18:10:02 -0800

    all good

--- lab13/js/lab13.js
+++ lab13/js/lab13.js
@@ -9,19 +9,48 @@ $(document).ready(function() {
   function fizzBuzz() {
     let oneLongString = '';
     for (let i = 1; i <= 200; i++) {
-      if (i % 3 == 0) {
-        console.log("fizz");
-        oneLongString += "Fizz\n"; 
-      } else if (i % 5 == 0) {
-        console.log("buzz");
-        oneLongString += "Buzz\n"; 
-     } else if (i % 7 == 0) { 
-       console.log("boom");
-       oneLongString += "Boom\n"; 
-     } else {     
-       console.log(i);
-       oneLongString += i + "\n"; 
-     }
+      if (i % 105 == 0) {
+        console.log("fizzbuzzboom");
+        oneLongString += "fizzbuzzboom\n"; 
+      } 
+      if (i % 174 == 0) {
+        console.log("fizzboombuzz");
+        oneLongString += "\n"; 
+     } 
+     if (i % 120 == 0) { 
+       console.log("fizzbuzzboom");
+       oneLongString += "fizzbuzzboom\n"; 
+     } 
+     else if (i % 56 == 0) {
+      console.log("boombuzz");
+      oneLongString += "boombuzz\n"; 
+    } else if (i % 40 == 0) {
+      console.log("buzzboom");
+      oneLongString += "buzzboom\n"; 
+   } else if (i % 35 == 0) { 
+     console.log("boombuzz");
+     oneLongString += "Boombuzz\n"; 
+   }
+   if (i % 21 == 0) {
+    console.log("fizzboom");
+    oneLongString += "fizzboom\n"; 
+  } else if (i % 15 == 0) {
+    console.log("fizzbuzz");
+    oneLongString += "fizzbuzz\n"; 
+  } else if (i % 7 == 0) {
+    console.log("boom");
+    oneLongString += "boom\n"; 
+ } else if (i % 5 == 0) { 
+   console.log("buzz");
+   oneLongString += "buzz\n"; 
+ }
+ if (i % 3 == 0) {
+  console.log("fizz");
+  oneLongString += "fizz\n"; 
+} else {     
+ console.log(i);
+ oneLongString += i + "\n"; 
+}
    }
 
    $("#output").append("<pre><p>" + oneLongString + "</p></pre>"); 
