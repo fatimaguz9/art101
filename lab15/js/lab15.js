a11fee9a928e04055d506faf4b6d2a36d6254953
fatimaguz9 <fafaguz2004@gmail.com>
2024-11-24 19:15:42 -0800

    Update lab15.js

--- lab15/js/lab15.js
+++ lab15/js/lab15.js
@@ -11,10 +11,25 @@ $.ajax({
     id:7
   }, 
   success: function (data) {
-    console.log(data);  
+    console.log(data);
+    var evChain = data.chain;
+    var pokemonNames = [];
+    
+    // Traverse and collect Pokémon names from each stage of the evolution chain
+    while (evChain) {
+      console.log(evChain.species.name);
+      pokemonNames.push(evChain.species.name); 
+      evChain = evChain.evolves_to[0];
+    }
+     document.getElementById("output").textContent = "Evolution Chain ${chainId}:" + pokemonNames.join(", ") + "\n";
   },
+
   error: function (jqXHR, textStatus, errorThrown) {
     console.error("AJAX Error:", textStatus, errorThrown); 
      }
   });
+});
+document.getElementById('fetchDataBtn').addEventListener('click', function() {
+  fetchPokemonEvolutionChain(7); // Fetch evolution chain for ID 7
+  fetchPokemonEvolutionChain(8); // Fetch evolution chain for ID 8
 });
